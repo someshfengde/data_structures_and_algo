b1ba6c7a9b2ce5061bb5da50dd51c477bad4e173
someshfengde <someshfengde@gmail.com>
2021-06-24 15:10:40 +0530

    lesson 1 to 5 completed

---
+++ pycodes/target_sum_arr.py
@@ -0,0 +1,34 @@
+#%%
+# my solution 
+def get_target_sum(arr,target):
+    a = 0 
+    if len(arr) <= 1 : 
+        return None 
+    
+    for val in range(len(arr)):
+        for bval in range(len(arr)- a):
+            if target == arr[a] + arr[a + bval]: 
+                return [a,a + bval]
+        a = a + 1 
+    return 'value_not_found'
+
+get_target_sum([1,4,5,6,7],5)
+
+#%%
+# Andrei's solution
+
+
+def andrei( arr, target):
+    for val in range(len(arr)): 
+        # print(val)
+        value_to_find = target - arr[val] 
+        for bval in range(len(arr) - val):
+            cval = val + bval 
+            if value_to_find == arr[cval]:
+                return [val,cval]
+                
+    return 'value_not_found'
+
+andrei(arr = [1,4,5,6,7],target = 800)
+
+# %%
